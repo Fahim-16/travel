5340680b94848f7b6d1f7716de79f9f8deb78e36
Fahim-16 <fahim.sakeer@gmail.com>
2022-07-07 23:52:45 +0530

    country  search created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
